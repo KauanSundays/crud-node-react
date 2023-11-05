f14f2991b6d3fa3b5b8be0400110184c770c2de4
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-04 23:33:36 -0300

    feat: wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import GlobalStyle from "./styles/global.js";
+import {toast, ToastContainer} from "react-toastify";
+import "react-toastify/dist/react-toastify.css";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+const Title = styled.h2``;
+
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Container>
+        <Title>USUARIOS</Title>
+        <Form />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle/>
+    </>
   );
 }
 
 export default App;
+// exportando pros outros
